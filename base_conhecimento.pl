b34f82a1e634faa5de4402e75dff100d0c9e90d3
Davi Kazuhiro <daviknatume@gmail.com>
2025-09-18 22:38:06 -0300

    adição dos fatos

---
+++ base_conhecimento.pl
@@ -0,0 +1,149 @@
+% ESPECIALIZACOES
+especializacao(computacao_quantica)
+especializacao(internet_of_things)
+especializacao(desenvolvimento_de_jogos)
+especializacao(ciencia_de_dados)
+especializacao(ciberseguranca)
+
+
+% TRILHAS
+trilha(computacao_quantica, 'Estudo e desenvolvimento de sistemas baseados nos principios da mecanica quantica.')  
+trilha(iot, 'Desenvolvimento de sistemas conectados envolvendo sensores, dispositivos embarcados e automacao.')  
+trilha(desenvolvimento_de_jogos, 'Design, programação gráfica e uso de engines especializadas para criacao de jogos digitais.')  
+trilha(ciencia_de_dados, 'Análise e interpretação de grandes volumes de dados para gerar insights e apoiar decisões.')
+trilha(ciberseguranca, 'Proteção de sistemas, redes e informações contra ataques e ameaças ciberneticas.')
+
+
+% HABILIDADES
+
+% COMPUTACAO QUANTICA
+habilidade(fisica)
+habilidade(matematica)
+habilidade(pesquisa)
+% IOT
+habilidade(eletronica)
+habilidade(solucoes)
+habilidade(automacao)
+% DESENVOLVIMENTO DE JOGOS
+habilidade(design)
+habilidade(user_experience)
+habilidade(criatividade)
+% CIENCIA DE DADOS
+habilidade(estatistica)
+habilidade(analise)
+habilidade(tomada_de_decisao)
+% CIBERSEGURANCA
+habilidade(criptografia)
+habilidade(investigacao_de_falhas)
+habilidade(raciocinio_logico)
+
+
+% PERFIS
+
+% COMPUTACAO QUANTICA
+perfil(computacao_quantica, fisica, 5)
+perfil(computacao_quantica, matematica, 5)
+perfil(computacao_quantica, pesquisa, 5)
+
+perfil(computacao_quantica, solucoes, 4)
+perfil(computacao_quantica, raciocinio_logico, 4)
+perfil(computacao_quantica, eletronica, 4)
+
+perfil(computacao_quantica, analise, 3)
+perfil(computacao_quantica, tomada_de_decisao, 3)
+perfil(computacao_quantica, criatividade, 3)
+
+perfil(computacao_quantica, automacao, 2)
+perfil(computacao_quantica, estatistica,2)
+perfil(computacao_quantica, investigacao_de_falhas, 2)
+
+perfil(computacao_quantica, design, 1)
+perfil(computacao_quantica, user_experience,1)
+perfil(computacao_quantica, criptografia, 1)
+
+% IOT
+perfil(internet_of_things, eletronica, 5)
+perfil(internet_of_things, automacao, 5)
+perfil(internet_of_things, solucoes, 5)
+
+perfil(internet_of_things, raciocinio_logico, 4)
+perfil(internet_of_things, analise, 4)
+perfil(internet_of_things, fisica, 4)
+
+perfil(internet_of_things, criatividade, 3)
+perfil(internet_of_things, matematica, 3)
+perfil(internet_of_things, pesquisa, 3)
+
+perfil(internet_of_things, estatistica, 2)
+perfil(internet_of_things, tomada_de_decisao, 2)
+perfil(internet_of_things, investigacao_de_falhas, 2)
+
+perfil(internet_of_things, design, 1)
+perfil(internet_of_things, user_experience, 1)
+perfil(internet_of_things, criptografia, 1)
+
+% DESENVOLVIMENTO DE JOGOS
+
+perfil(desenvolvimento_de_jogos, criatividade, 5)
+perfil(desenvolvimento_de_jogos, design, 5)
+perfil(desenvolvimento_de_jogos, user_experience, 5)
+
+perfil(desenvolvimento_de_jogos, raciocinio_logico, 4)
+perfil(desenvolvimento_de_jogos, analise, 4)
+perfil(desenvolvimento_de_jogos, matematica, 4)
+
+perfil(desenvolvimento_de_jogos, solucoes, 3)
+perfil(desenvolvimento_de_jogos, automacao, 3)
+perfil(desenvolvimento_de_jogos, pesquisa, 3)
+
+perfil(desenvolvimento_de_jogos, fisica, 2)
+perfil(desenvolvimento_de_jogos, tomada_de_decisao, 2)
+perfil(desenvolvimento_de_jogos, estatistica, 2)
+
+perfil(desenvolvimento_de_jogos, eletronica, 1)
+perfil(desenvolvimento_de_jogos, criptografia, 1)
+perfil(desenvolvimento_de_jogos, investigacao_de_falhas, 1)
+
+% CIENCIA DE DADOS
+
+perfil(bigdata, estatistica, 5)
+perfil(bigdata, analise, 5)
+perfil(bigdata, tomada_de_decisao, 5)
+
+perfil(bigdata, matematica, 4)
+perfil(bigdata, raciocinio_logico, 4)
+perfil(bigdata, pesquisa, 4)
+
+perfil(bigdata, solucoes, 3)
+perfil(bigdata, automacao, 3)
+perfil(bigdata, criatividade, 3)
+
+perfil(bigdata, fisica, 2)
+perfil(bigdata, design, 2)
+perfil(bigdata, user_experience, 2)
+
+perfil(bigdata, criptografia, 1)
+perfil(bigdata, eletronica, 1)
+perfil(bigdata, investigacao_de_falhas, 1)
+
+% CIBERSEGURANCA
+
+perfil(ciberseguranca, criptografia, 5)
+perfil(ciberseguranca, investigacao_de_falhas, 5)
+perfil(ciberseguranca, raciocinio_logico, 5)
+
+perfil(ciberseguranca, analise, 4)
+perfil(ciberseguranca, solucoes, 4)
+perfil(ciberseguranca, pesquisa, 4)
+
+perfil(ciberseguranca, tomada_de_decisao, 3)
+perfil(ciberseguranca, estatistica, 3)
+perfil(ciberseguranca, matematica, 3)
+
+perfil(ciberseguranca, automacao, 2)
+perfil(ciberseguranca, fisica, 2)
+perfil(ciberseguranca, criatividade, 2)
+
+perfil(ciberseguranca, eletronica, 1)
+perfil(ciberseguranca, design, 1)
+perfil(ciberseguranca, user_experience, 1)
